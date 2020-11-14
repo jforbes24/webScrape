1475f1a0a233e8055ada37674ac7542791a7009d
jforbes24 <64039648+jforbes24@users.noreply.github.com>
2020-11-14 22:49:57 +0000

    Add files via upload

--- webScrape.py
+++ webScrape.py
@@ -5,6 +5,7 @@ import random
 import numpy as np
 import pandas as pd
 
+# assign user-agent
 user_agent_list = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15',
@@ -17,48 +18,83 @@ user_agent_list = [
 baseurl = 'https://www.diy.com/'
 url = "https://www.diy.com/departments/lighting/indoor-lights/floor-lamps/DIY579453.cat?page={x}"
 
-"""
-
 productlinks = []
 
-for x in range(1,6):
-    for i in range(1,6):
-        # pick a random user agent
-        user_agent = random.choice(user_agent_list)
-        # set the headers
-        headers = {'User-Agent' : user_agent}
-        # make the request
-        result = requests.get(f"https://www.diy.com/departments/lighting/indoor-lights/floor-lamps/DIY579453.cat?page={x}", headers=headers)    
+for i in range(1,6):
+    # pick a random user agent
+    user_agent = random.choice(user_agent_list)
+    # set the headers
+    headers = {'User-Agent' : user_agent}
+
+# make the request
+for x in range(1,4):
+    result = requests.get(f"https://www.diy.com/departments/flooring-tiling/flooring-underlay/laminate-flooring/DIY566433.cat?page={x}", headers=headers)    
     soup = bs4.BeautifulSoup(result.content, 'lxml')
     productlist = soup.find_all('li', class_='b9bdc658')
-
-    result_df = pd.DataFrame()
-
     for item in productlist:
         for link in item.find_all('a', href=True):
             # print(link['href'])
             productlinks.append(baseurl + link['href'])
+            
+print(result.status_code)
+print(len(productlinks))
 
-print(productlinks)
+# testlink = 'https://www.diy.com/departments/dartmoor-oak-effect-laminate-flooring-1-48m-pack/5013599012878_BQ.prd'
 
-"""
+productData = []
 
-# get product info
-testlink = 'https://www.diy.com//departments/goodhome-apennin-matt-cream-floor-light/5036581098452_BQ.prd'
-headers = {
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
-    }
-res = requests.get(testlink, headers=headers)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.content, 'lxml')
+for link in productlinks:
+    r = requests.get(link, headers=headers)
+    soup = bs4.BeautifulSoup(r.content, 'lxml')
 
+    # get name
+    name = soup.find('h1', class_='ccb9d67a _17d3fa36 _1c13b5e2 _58b3d2d9 _514c3e90 _75d33510 _266816c0 _6ba14bc3 fcf8ebfc _78852320 bae4848b cc6bbaee _23ee746f').text.strip()
+    for tr in soup.find_all('tr')[2:]:
+        td = tr.find_all('th')
 
-# get product name
-name = soup.select('h1')[0].getText().strip()
-print(name)
+    # get sku
+    table = soup.find('table')
+    tableRows = table.find_all('tr')
 
-# get product price
-for string in soup.stripped_strings:
-    print(repr(string))
-# get product rating
-print(soup.title.text)
+    for tr in tableRows:
+        td = tr.find_all('td')
+        for i in td:
+            row = i.text
+    # get rating
+    try:
+        rating = soup.find('div', class_='_45e852d0 _6418d197 _2263bdd0').text.strip()
+    except:
+        rating = 'no rating'
+        
+    # get reviews
+    try:
+        reviews = soup.find('span', class_='ccb9d67a _17d3fa36 _50344329 b1bfb616 cc6bbaee').text.strip()
+    except:
+        reviews = 'no reviews'
+
+    # get price
+    price = soup.find('div', class_='b25ad5d5 _4e80f7be _23ee746f _7b343263 _21dc035c').text.strip()
+
+    # get unit price
+    unitPrice = soup.find('div', class_='b00398fe b1bfb616 _8da52348 b1bfb616').text
+
+    floor = {
+        'sku': row,
+        'name': name,
+        'rating': rating,
+        'reviews': reviews,
+        'price': price,
+        'unitPrice': unitPrice
+        }
+        
+    productData.append(floor)
+
+print(len(productData))
+
+df = pd.DataFrame(productData)
+pd.set_option('display.max_columns', 100)
+
+# save to excel
+df.to_excel('bs4Floor.xlsx', index=True, header=True)
+data = pd.read_excel('/Users/jforbes84/PycharmProjects/bs4Floor.xlsx')
+print(df.head)
