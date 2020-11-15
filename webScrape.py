61944e002ce24a7ce2b6cd229fa0d26ced285785
jforbes24 <64039648+jforbes24@users.noreply.github.com>
2020-11-15 01:24:45 +0000

    Add files via upload

--- webScrape.py
+++ webScrape.py
@@ -46,45 +46,41 @@ productData = []
 for link in productlinks:
     r = requests.get(link, headers=headers)
     soup = bs4.BeautifulSoup(r.content, 'lxml')
-
     # get name
     name = soup.find('h1', class_='ccb9d67a _17d3fa36 _1c13b5e2 _58b3d2d9 _514c3e90 _75d33510 _266816c0 _6ba14bc3 fcf8ebfc _78852320 bae4848b cc6bbaee _23ee746f').text.strip()
     for tr in soup.find_all('tr')[2:]:
         td = tr.find_all('th')
-
     # get sku
     table = soup.find('table')
     tableRows = table.find_all('tr')
 
     for tr in tableRows:
-        td = tr.find_all('td')
-        for i in td:
+        td = tr.find_all('td')    
+        for i in td: 
             row = i.text
     # get rating
     try:
         rating = soup.find('div', class_='_45e852d0 _6418d197 _2263bdd0').text.strip()
     except:
         rating = 'no rating'
-        
     # get reviews
     try:
         reviews = soup.find('span', class_='ccb9d67a _17d3fa36 _50344329 b1bfb616 cc6bbaee').text.strip()
     except:
         reviews = 'no reviews'
-
     # get price
     price = soup.find('div', class_='b25ad5d5 _4e80f7be _23ee746f _7b343263 _21dc035c').text.strip()
-
     # get unit price
     unitPrice = soup.find('div', class_='b00398fe b1bfb616 _8da52348 b1bfb616').text
-
+    
     floor = {
         'sku': row,
         'name': name,
         'rating': rating,
         'reviews': reviews,
         'price': price,
-        'unitPrice': unitPrice
+        'unitPrice': unitPrice,
+        'link': link
         }
         
     productData.append(floor)
@@ -95,6 +91,6 @@ df = pd.DataFrame(productData)
 pd.set_option('display.max_columns', 100)
 
 # save to excel
-df.to_excel('bs4Floor.xlsx', index=True, header=True)
+df.to_excel('bs4Floor.xlsx', index=False, header=True)
 data = pd.read_excel('/Users/jforbes84/PycharmProjects/bs4Floor.xlsx')
 print(df.head)
